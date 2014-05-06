910dac1c79d02b94a5dade42c37f3099f2fe455d
steri <sr-richi@web.de>
2014-05-06 09:22:03 +0200

    Non-JAXB-Example hinzugefügt.

---
+++ src/main/java/de/steri/springbatch/xml/nonjaxbreader/processing/CustomXmlItemReader.java
@@ -0,0 +1,10 @@
+package de.steri.springbatch.xml.nonjaxbreader.processing;
+
+import org.springframework.batch.item.xml.StaxEventItemReader;
+
+public class CustomXmlItemReader extends StaxEventItemReader<Object> {
+	
+	
+	
+	
+}
